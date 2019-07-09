bedc9e25f2753b1068c1c8eff41daa1201ef3cf8
Dwayne7889 <dwaynen0708@gmail.com>
2019-07-09 23:33:53 +0200

    2019 update

---
+++ DwaynePortfolio/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace DwaynePortfolio.Controllers
+{
+   
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Fullstack()
+        {
+            return View();
+        }
+
+        public ActionResult History()
+        {
+            return View();
+        }
+    }
+}
